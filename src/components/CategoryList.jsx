b44393b6c2adcdef474b4969f4edc0f41bff9329
PremKumar <109847674+prem2230@users.noreply.github.com>
2025-06-24 11:49:54 +0530

    feat: update CategoryList component to use unique keys and correct media image source

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -13,9 +13,9 @@ const CategoryList = async () => {
         <div className="px-4 overflow-x-scroll scrollbar-hide">
             <div className="flex gap-4 md:gap-8 ">
                 {items?.items?.map((item) => (
-                    <Link key={item.id} href={`/list?cat=${item?.slug}`} className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                    <Link key={`${item.id}+${item.slug}`} href={`/list?cat=${item?.slug}`} className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
                         <div className="relative bg-slate-100 w-full h-96">
-                            <Image src={item?.media?.mainImage?.image} alt="bottle-product" fill sizes="20vw" className="object-cover" />
+                            <Image src={item?.media?.mainMedia?.image?.url || "/products/bottle-product.jpg"} alt="bottle-product" fill sizes="20vw" className="object-cover" />
                         </div>
                         <h1 className="mt-8 font-light text-center tracking-wide"> {item?.name} </h1>
                     </Link>
