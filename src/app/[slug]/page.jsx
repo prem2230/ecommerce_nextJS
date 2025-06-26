385ef9d57336aaedc0d3979da218465cd9ff7e84
PremKumar <109847674+prem2230@users.noreply.github.com>
2025-06-26 23:54:32 +0530

    feat: update SinglePage and ProductList components to fetch and display product details dynamically

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,38 +1,51 @@
+import { wixClientServer } from "@/lib/wixClientServer";
 import Add from "../../components/Add"
 import CustomizeProducts from "../../components/CustomizeProducts"
 import ProductImages from "../../components/ProductImages"
 
-const SinglePage = () => {
+const SinglePage = async ({ params }) => {
+  const id = params.slug
+
+  const wixClient = await wixClientServer();
+
+  const res = await wixClient.products.getProduct(id);
+
+  console.log('first', res)
+
   return (
     <div className="px-4 md:px-8 lg:px-8 xl:px-16 2xl:px-32 relative flex flex-col lg:flex-row gap-16">
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
         <ProductImages />
       </div>
       <div className="w-full lg:w-1/2 flex flex-col gap-6">
-        <h1 className="text-4xl font-medium">Product Name</h1>
-        <p className="text-gray-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam ducimus dolor magnam quaerat quae, fugiat pariatur natus eaque laudantium eos quidem quas itaque iste ut corporis. Iure sint quas vitae!
+        <h1 className="text-4xl font-medium">{res.product.name}</h1>
+        <p className="text-gray-500">{res.product.description}
         </p>
         <div className="h-[2px] bg-gray-100" />
         <div className="flex items-center gap-4">
-          <h3 className="text-xl text-gray-500 line-through">₹100</h3>
-          <h2 className="text-2xl font-medium">₹50</h2>
+          <h3 className="text-xl text-gray-500 line-through">{res.product.price.formatted.price}</h3>
+          <h2 className="text-2xl font-medium">{res.product.price.formatted.discountedPrice}</h2>
         </div>
         <div className="h-[2px] bg-gray-100" />
         <CustomizeProducts />
         <Add />
         <div className="h-[2px] bg-gray-100" />
-        <div className="text-sm">
-          <h4 className=" font-bold mb-4">PRODUCT INFO </h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officiis dolores, vero numquam quidem architecto tempore itaque minus. Quae qui porro veniam eum deserunt, quam dicta quisquam hic aperiam rem.</p>
-        </div>
-        <div className="text-sm">
+        {res.product.additionalInfoSections.map((section, index) => (
+          <div className="text-sm" key={index}>
+            <h4 className="font-bold mb-4">{section.title}</h4>
+            <p>{section.description}</p>
+            {/* <h4 className=" font-bold mb-4">PRODUCT INFO </h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officiis dolores, vero numquam quidem architecto tempore itaque minus. Quae qui porro veniam eum deserunt, quam dicta quisquam hic aperiam rem.</p> */}
+          </div>
+        ))}
+        {/* <div className="text-sm">
           <h4 className=" font-bold mb-4">SPECIFICATION</h4>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officiis dolores, vero numquam quidem architecto tempore itaque minus. Quae qui porro veniam eum deserunt, quam dicta quisquam hic aperiam rem.</p>
         </div>
         <div className="text-sm">
           <h4 className=" font-bold mb-4">REFUND POLICY </h4>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officiis dolores, vero numquam quidem architecto tempore itaque minus. Quae qui porro veniam eum deserunt, quam dicta quisquam hic aperiam rem.</p>
-        </div>
+        </div> */}
       </div>
     </div>
   )
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -14,7 +14,7 @@ const ProductList = async ({ categoryId = "a707b5d2-3a96-2d0b-9a3a-e4bab712d479"
     return (
         <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
             {res.items.map((product) => (
-                <Link key={product._id} href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+                <Link key={product._id} href={`/${product._id}`} className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
                     <div className="relative w-full h-80">
                         <Image src={product?.media?.items[0]?.image?.url || "/products/men-outfit.jpg"} alt="product" fill sizes="25vw" className="rounded-md absolute object-cover z-10 hover:opacity-0 transition-opacity ease-in duration-500" />
                         <Image src={product?.media?.items[1]?.image?.url || "/products/men-outfit.jpg"} alt="product" fill sizes="25vw" className="rounded-md absolute object-cover" />
